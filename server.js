98f34909c91a41cfa6a28c1716d65215816dd5b0
ICVRXS <calebhay@rocketmail.com>
2020-07-28 18:28:49 -0700

    server set up

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = procrss.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.useexpress.static("public");
+app.use(express.static("public"));
 
-mongoose.connect(process.env)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
-app.use(require("./routes"))
+app.use(require("./routes"));
+
+app.listen(PORT, ()=>{
+  console.log(`App is running on port: ${PORT}`)
+});
